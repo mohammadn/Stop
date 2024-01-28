502fca22fc147a8e4522fd69cf4baee19dbef7c3
Mohammadn <mohammadnajafzade@yahoo.com>
2024-01-28 13:54:24 +0000

    init commit

---
+++ Stop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stop
+//
+//  Created by Mohammad Najafzadeh on 28/01/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello World!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stop/StopApp.swift
@@ -0,0 +1,19 @@
+//
+//  StopApp.swift
+//  Stop
+//
+//  Created by Mohammad Najafzadeh on 28/01/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StopApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
